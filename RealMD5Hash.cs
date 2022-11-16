422a22be2e0e76286665c30407af976540927036
Marco Capelli <andreadoria1@gmail.com>
2022-11-16 10:48:09 +0100

    Create RealMD5Hash.cs

---
+++ RealMD5Hash.cs
@@ -0,0 +1,21 @@
+using System.Security.Cryptography;
+using System.Text;
+
+public static class RealMD5Hash
+{
+    public static string MD5Php(string input)
+    {
+        // Form hash
+        MD5 h = MD5.Create();
+        byte[] data = h.ComputeHash(Encoding.Default.GetBytes(input));
+
+        // Create string representation
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < data.Length; ++i)
+        {
+            sb.Append(data[i].ToString("x2"));
+        }
+        return sb.ToString();
+    }
+}
