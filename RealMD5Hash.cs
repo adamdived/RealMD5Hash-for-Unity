e7c75b8cf3067d01644b230846d289853b686901
Marco Capelli <andreadoria1@gmail.com>
2022-11-16 11:06:01 +0100

    Update RealMD5Hash.cs

--- RealMD5Hash.cs
+++ RealMD5Hash.cs
@@ -5,11 +5,11 @@ public static class RealMD5Hash
 {
     public static string MD5Php(string input)
     {
-        // Form hash
+        // Input form to hash
         MD5 h = MD5.Create();
         byte[] data = h.ComputeHash(Encoding.Default.GetBytes(input));
 
-        // Create string representation
+        // Create the correct MD5 Hash string representation
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < data.Length; ++i)
